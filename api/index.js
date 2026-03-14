13031c3b2c62d9f7c7e543814c500a33523bbc2a
Aditri Sharma <aditrisharma2005@gmail.com>
2026-03-14 16:54:04 +0530

    chaging path

--- api/index.js
+++ api/index.js
@@ -13,8 +13,8 @@ app.use(express.static("../public"));
 const __dirname = dirname(fileURLToPath(import.meta.url));
 app.set("views", path.join( __dirname + "../views"));
 app.set("view engine", "ejs");
-
-app.use(bodyParser.urlencoded({extended:true}));
+app.use(express.static(path.join(__dirname, "../public")));
+//app.use(bodyParser.urlencoded({extended:true}));
 
 app.get('/',(req,res)=>{
     res.render("index",{
